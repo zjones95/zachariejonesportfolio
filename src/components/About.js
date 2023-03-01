ce5529c45915e44886d3f923c29ea6a8abd37db5
unknown <zachariejones@gmail.com>
2023-03-01 10:33:02 -0800

    Update deps, update home-about and about copy

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ function About() {
             <div className="about-container">
                 <img src="./about-image.png" alt="about"></img>
                 <h2>Hi, I'm Zacharie Jones</h2>
-                <p>I'm a Full-Stack Web Developer from San Jose, CA. I love building modern web applications that make people's lives easier. I work primarily with React, Node, Express, GraphQL, and MongoDB. My previous background was as an IT Consultant, Ecommerce Business Owner, and Digital Marketer (Paid Media and SEO).</p>
+                <p>I work primarily with React, and I've worked on projects involving data visualization, chrome extensions, Zoom apps, Stripe, and much more. My previous career was as an IT Consultant, Ecommerce Business Owner, and Digital Marketer (Paid Media and SEO). I also love working with CSS (which most people don't love).</p>
                 <p>I'm passionate about solving problems and improving the lives of others through software engineering. I'm currently looking for roles as a Front-End or Full-Stack Web Developer, and I'm always interested in talking to other developers. If you'd like to get in touch, please don't hesitate to contact me.</p>
                 <Link to="/contact">Contact</Link>
             </div>
--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -6,10 +6,10 @@ function HomeAbout() {
             <div className="home-about-text">
                 <span className="section-span"></span>
                 <h2 className="section-header">About Me</h2>
-                <p>Hi, my name's Zach Jones, a Full-Stack Web Developer from San Jose, CA. I love building websites that solve life's unique problems.</p>
+                <p>Hi, my name's Zach Jones, a Front End Developer from San Jose, CA. I love building websites that solve life's unique problems.</p>
                 <br />
                 <p> 
-                    I work primarily with MongoDB, Express, React, Node, and GraphQL and have set up countless WordPress and Shopify websites. My previous career was as an IT Consultant, Ecommerce Business Owner, and Digital Marketer (Paid Media and SEO). I also really enjoy tinkering with CSS.
+                    I work primarily with React, and I've worked on projects involving data visualization, chrome extensions, Zoom apps, Stripe, and much more. My previous career was as an IT Consultant, Ecommerce Business Owner, and Digital Marketer (Paid Media and SEO). I also love working with CSS (which most people don't love).
                 </p>
             </div>
             <div className="home-about-image">
